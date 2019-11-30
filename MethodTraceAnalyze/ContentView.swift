49a2737a799a9c6437edd6e98aa50db5210c48a0
戴铭 <ming1016@foxmail.com>
2019-11-30 21:14:47 +0800

    first push

---
+++ MethodTraceAnalyze/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MethodTraceAnalyze
+//
+//  Created by ming on 2019/11/30.
+//  Copyright © 2019 ming. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
